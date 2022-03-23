8a662910f82b169e3891568176e8b87e3f1afe4f
Garland Kan <garland@managedkube.com>
2022-03-23 14:38:07 +0000

    Fixing bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "Running TestSuite: ${TEST_SUITE_NAME}"
 time=$(date)
 # echo "::set-output name=time::$time"
 
-# aws eks update-kubeconfig --name ops
+aws eks update-kubeconfig --name ops
 
 kubectl version
 
@@ -20,6 +20,9 @@ kubectl testkube run testsuite ${TEST_SUITE_NAME} -f
 # Get last test status
 LAST_TEST_STATUS=$(kubectl testkube get testsuiteexecution -o json | jq -r '.results[0].status')
 
+# Output status based on the test results
+# Running a the "testkube run testsuite" always produces an exit code of 0 even if there is a failure in it
+# We have to explicitly grab the output and set the error code
 if [[ "${LAST_TEST_STATUS}" == "error" ]]; then
   exit 1
 else
